c9ed517f2fac35015a3aa97a1aa783aa24cd1664
Stanislav Yanakiev <stan5050@gmail.com>
2023-05-29 12:13:42 +0300

    'get info and export to a .txt file'

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ import requests
 website = 'https://subslikescript.com/movie/Titanic-120338'
 result = requests.get(website)
 content = result.text
+# print(content)
 
 soup = BeautifulSoup(content, 'lxml')
-print(soup.prettify())
 
+box = soup.find('article', class_='main-article')
 
+# Get the movie title
+title = box.find('h1').get_text()
+print(title)
+
+# Get the subtitles
+# transcript = box.find('div', class_='full-script').get_text()
+# Add (strip=True, separator=' ')  for exporting to txt files
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+# Export to a txt file:
+with open(f'{title}.txt', 'w', encoding="utf-8") as file:
+    file.write(transcript)
